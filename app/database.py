bdc399b469d56ac4d183f9165bb717ea933bb920
Dmytro Shyrokov <sh.dmytro@protonmail.com>
2024-03-05 14:05:21 +0100

    Upated .gitignore file.

--- app/database.py
+++ app/database.py
@@ -5,6 +5,7 @@ from .config import settings
 
 DATABASE_URL = f"postgresql://{settings.postgre_usr}:{settings.postgre_pwd}@{settings.postgre_host}:{settings.postgre_port}/{settings.database_name}"
 
+
 engine = create_engine(DATABASE_URL)
 
 local_session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
@@ -16,14 +17,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-
-# while True:
-#     try:
-#         conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="qw34.rhju780!", cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         break
-#     except Exception as error:
-#         print("Error while connection database:\n", error)
-#         time.sleep(2)
